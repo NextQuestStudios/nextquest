1e96ac16c6cd3ff3d2fb72d890029b74e567be23
OneManMediaUk <onemanmedialimited@gmail.com>
2025-04-12 04:08:16 +0100

    Delete public directory

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-// Back to Top Button
-document.getElementById("backToTop").addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
-// Admin Login Button
-document.getElementById("adminLoginBtn").addEventListener("click", () => {
-  window.location.href = "../admin/login.html";
-});
